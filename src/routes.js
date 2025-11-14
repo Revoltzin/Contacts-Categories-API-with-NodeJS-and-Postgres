1320df9bd7d3cebd7a602d0d82707b534a2cb6fb
Revoltzin <jat1963@gmail.com>
2025-11-14 00:51:46 -0300

    Fix router initialization in routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,9 @@ const { Router } = require("express")
 
 const ContactController = require("./app/controller/ContactController.js")
 
-const router = Router
+const router = Router()
 
 router.get("/contacts", ContactController.index)
+router.get("/contacts/:id", ContactController.show)
 
 module.exports = router
