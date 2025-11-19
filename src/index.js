740b45dc777e2068dac8b506b51accc27f3236ae
Revoltzin <jat1963@gmail.com>
2025-11-19 01:29:41 -0300

    Fix error handling middleware placement in index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ const express = require("express")
 
 const routes = require("./routes.js")
 
-const app = express() 
-app.use(express.json())
+const app = express()
 
+app.use(express.json())
 app.use(routes)
+app.use((error, req, res, next) => {
+    console.log(error)
+    res.sendStatus(500)
+})
 
 app.listen(3000, () => {
     console.log("Serer is running")
